37e555f70377d1712fdc25a739530771ded60142
Montyyy <danyramadhan558@gmail.com>
2026-08-02 18:44:04 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // mongoose.connect() mengembalikan sebuah Promise,
-    // makanya kita "await" agar server baru jalan setelah DB benar-benar tersambung.
+   
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`✅ MongoDB terhubung: ${conn.connection.host}`);
