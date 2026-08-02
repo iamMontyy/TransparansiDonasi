64cfb8bdccbd497d6d0248d4407ee966fd5837e3
Montyyy <danyramadhan558@gmail.com>
2026-08-02 18:44:23 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
    const conn = await mongoose.connect(process.env.MONGO_URI);
-console.log(`✅ MongoDB terhubung: ${conn.connection.host}`);
+console.log(` MongoDB terhubung: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ Gagal konek ke MongoDB: ${error.message}`);
+    console.error(` Gagal konek ke MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
