005ca74148ada2fc78fbbfcdc10dad35aea56550
Montyyy <danyramadhan558@gmail.com>
2026-08-05 20:39:24 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema(
       required: [true, 'Nama wajib diisi'],
       trim: true, 
     },
-    email: {
+    email: 
       type: String,
       required: [true, 'Email wajib diisi'],
       unique: true, 
