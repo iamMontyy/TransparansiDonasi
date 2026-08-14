113b00490fbbd490ba0a6e967048c4173094d955
Montyyy <danyramadhan558@gmail.com>
2026-08-14 18:07:15 +0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const router = express.Router(); // "mini app" khusus untuk kelompok route ini
+const router = express.Router(); 
 const { register, login } = require('../controllers/authController');
-
-// Setiap route di sini otomatis diawali prefix '/api/auth'
-// (prefix ini kita atur nanti di server.js)
 router.post('/register', register);
 router.post('/login', login);
 
