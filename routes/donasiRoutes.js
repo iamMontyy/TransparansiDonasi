9adeae475f4036e276e6d71b187ce1ac3303ecdf
Montyyy <danyramadhan558@gmail.com>
2026-08-14 18:07:26 +0700

    Update donasiRoutes.js

--- routes/donasiRoutes.js
+++ routes/donasiRoutes.js
@@ -7,14 +7,8 @@ const {
   updateDonasi,
 } = require('../controllers/donasiController');
 const { protect, authorize } = require('../middleware/auth');
-
-// --- Route PUBLIK (tanpa middleware "protect") ---
 router.get('/', getSemuaDonasi);
 router.get('/:id', getDonasiById);
-
-// --- Route PRIVAT (wajib login, pakai middleware "protect") ---
-// Express menjalankan middleware secara berurutan dari kiri ke kanan,
-// jadi "protect" akan dicek dulu sebelum masuk ke fungsi "buatDonasi".
 router.post('/', protect, authorize('donatur', 'admin'), buatDonasi);
 router.put('/:id', protect, authorize('donatur', 'admin'), updateDonasi);
 
