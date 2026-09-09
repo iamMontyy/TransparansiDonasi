17afcc9bbaaa23f877b3c846219f25b4a2b24b7a
Montyyy <danyramadhan558@gmail.com>
2026-09-09 19:26:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const donasiRoutes = require('./routes/donasiRoutes');
 const penyaluranRoutes = require('./routes/penyaluranRoutes');
 connectDB();
 const app = express();
-app.use(cors()); 
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
