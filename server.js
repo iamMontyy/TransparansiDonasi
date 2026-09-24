a258ae8edbafd82e3d746a94c0372b3e81468ced
Montyyy <danyramadhan558@gmail.com>
2026-09-24 23:08:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db')
 const authRoutes = require('./routes/authRoutes');
 const donasiRoutes = require('./routes/donasiRoutes');
 const penyaluranRoutes = require('./routes/penyaluranRoutes');
